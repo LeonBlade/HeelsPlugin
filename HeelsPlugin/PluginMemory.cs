59d6d084454bcedd3a32d5b0faf15dc32b816f8f
James Stine <leon.blade@gmail.com>
2020-12-24 03:12:57 -0500

    Use the proper function for writing the model position

--- HeelsPlugin/PluginMemory.cs
+++ HeelsPlugin/PluginMemory.cs
@@ -13,14 +13,14 @@ namespace HeelsPlugin
 
 		public IntPtr playerMovementFunc;
 		[UnmanagedFunctionPointer(CallingConvention.StdCall)]
-		public delegate void PlayerMovementDelegate(IntPtr player, float x, float y, float z, long u);
+		public delegate void PlayerMovementDelegate(IntPtr player);
 		public readonly Hook<PlayerMovementDelegate> playerMovementHook;
 
 		public PluginMemory(DalamudPluginInterface pluginInterface)
 		{
 			this.pi = pluginInterface;
 
-			this.playerMovementFunc = this.pi.TargetModuleScanner.ScanText("40 53 48 83 EC 20 F3 0F 11 89 A0");
+			this.playerMovementFunc = this.pi.TargetModuleScanner.ScanText("48 89 5C 24 08 55 48 8B EC 48 83 EC 70 83 79 7C 00");
 			this.playerMovementHook = new Hook<PlayerMovementDelegate>(
 				playerMovementFunc,
 				new PlayerMovementDelegate(PlayerMovementHook)
@@ -49,18 +49,32 @@ namespace HeelsPlugin
 			}
 		}
 
-		private void PlayerMovementHook(IntPtr player, float x, float y, float z, long u)
+		[StructLayout(LayoutKind.Sequential)]
+		struct Vector3
 		{
+			public float X;
+			public float Y;
+			public float Z;
+		}
+
+		private void PlayerMovementHook(IntPtr player)
+		{
+			// Call the original function.
+			this.playerMovementHook.Original(player);
+
 			try
 			{
-			
 				if (this.pi.ClientState.Actors.Length > 0 && this.pi.ClientState.Actors[0].Address == player)
-					y += offset;
+				{
+					var model = new IntPtr(Marshal.ReadInt64(player, 0xF0) + 0x50);
+					var position = Marshal.PtrToStructure<Vector3>(model);
+
+					position.Y += offset;
+
+					Marshal.StructureToPtr(position, model, false);
+				}
 			}
 			catch { }
-
-			// Call the original function.
-			this.playerMovementHook.Original(player, x, y, z, u);
 		}
 	}
 }
